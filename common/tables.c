dcafe434d68ff3c06d59bf4124c010643fb3703a
Pepijn Van Eeckhoudt <pepijn.vaneeckhoudt@luciad.com>
2013-05-15 16:32:54 +0200

    Updated table definitions to 0.5.4 specification

--- common/tables.c
+++ common/tables.c
@@ -25,13 +25,13 @@ static column_info_t geopackage_contents_columns[] = {
         {"table_name", "text", N, SQL_PRIMARY_KEY | SQL_NOT_NULL, NULL},
         {"data_type", "text", N, SQL_NOT_NULL, NULL},
         {"identifier", "text", N, SQL_NOT_NULL, NULL},
-        {"description", "text", N, SQL_NOT_NULL, NULL},
+        {"description", "text", T(""), SQL_NOT_NULL, NULL},
         {"last_change", "text", F("strftime('%Y-%m-%dT%H:%M:%fZ', 'now')"), SQL_NOT_NULL, NULL},
-        {"min_x", "double", D(-180.0), SQL_NOT_NULL, NULL},
-        {"min_y", "double", D(-90.0), SQL_NOT_NULL, NULL},
-        {"max_x", "double", D(180.0), SQL_NOT_NULL, NULL},
-        {"max_y", "double", D(90.0), SQL_NOT_NULL, NULL},
-        {"srid", "integer", I(0), SQL_NOT_NULL, "REFERENCES spatial_ref_sys(srid)"},
+        {"min_x", "double", N, SQL_NOT_NULL, NULL},
+        {"min_y", "double", N, SQL_NOT_NULL, NULL},
+        {"max_x", "double", N, SQL_NOT_NULL, NULL},
+        {"max_y", "double", N, SQL_NOT_NULL, NULL},
+        {"srid", "integer", N, SQL_NOT_NULL, "REFERENCES spatial_ref_sys(srid)"},
 };
 table_info_t geopackage_contents = {
         "geopackage_contents",
@@ -60,24 +60,22 @@ static column_info_t geometry_columns_columns[] = {
         {"f_table_name", "text", N, SQL_PRIMARY_KEY, "REFERENCES geopackage_contents(table_name)"},
         {"f_geometry_column", "text", N, SQL_PRIMARY_KEY, NULL},
         {"geometry_type", "integer", N, SQL_NOT_NULL, NULL},
+        {"coord_dimension", "integer", N, SQL_NOT_NULL, NULL},
         {"srid", "integer", N, SQL_NOT_NULL, "REFERENCES spatial_ref_sys(srid)"}
 };
 table_info_t geometry_columns = {
         "geometry_columns",
-        geometry_columns_columns, 4,
+        geometry_columns_columns, 5,
         NULL, 0
 };
 
 static column_info_t raster_columns_columns[] = {
         {"r_table_name", "text", N, SQL_PRIMARY_KEY, "REFERENCES geopackage_contents(table_name)"},
-        {"r_raster_column", "text", N, SQL_PRIMARY_KEY, NULL},
-        {"compr_qual_factor", "integer", I(-1), SQL_NOT_NULL, NULL},
-        {"georectification", "integer", I(-1), SQL_NOT_NULL, NULL},
-        {"srid", "integer", N, SQL_NOT_NULL, "REFERENCES spatial_ref_sys(srid)"}
+        {"r_raster_column", "text", N, SQL_PRIMARY_KEY, NULL}
 };
 table_info_t raster_columns = {
         "raster_columns",
-        raster_columns_columns, 5,
+        raster_columns_columns, 2,
         NULL, 0
 };
 
@@ -117,24 +115,25 @@ static column_info_t tiles_columns[] = {
 
 static column_info_t xml_metadata_columns[] = {
         {"id", "integer", N, SQL_PRIMARY_KEY, NULL},
-        {"md_scope", "text", T("undefined"), SQL_NOT_NULL, NULL},
+        {"md_scope", "text", T("dataset"), SQL_NOT_NULL, NULL},
         {"md_standard_uri", "text", T("http://schemas.opengis.net/iso/19139"), SQL_NOT_NULL, NULL},
+        {"mime_type", "text", T("text/xml"), SQL_NOT_NULL, NULL},
         {"metadata", "text", T(""), SQL_NOT_NULL, NULL}
 };
 table_info_t xml_metadata = {
         "xml_metadata",
-        xml_metadata_columns, 4,
+        xml_metadata_columns, 5,
         NULL, 0
 };
 
 static column_info_t metadata_reference_columns[] = {
-        {"reference_scope", "text", T("table"), SQL_NOT_NULL, NULL},
-        {"table_name", "text", T("undefined"), SQL_NOT_NULL, NULL},
-        {"column_name", "text", T("undefined"), SQL_NOT_NULL, NULL},
-        {"row_id_value", "integer", I(0), SQL_NOT_NULL, NULL},
+        {"reference_scope", "text", N, SQL_NOT_NULL, NULL},
+        {"table_name", "text", N, 0, NULL},
+        {"column_name", "text", N, 0, NULL},
+        {"row_id_value", "integer", N, 0, NULL},
         {"timestamp", "text", F("strftime('%Y-%m-%dT%H:%M:%fZ', 'now')"), SQL_NOT_NULL, NULL},
-        {"md_file_id", "integer", I(0), SQL_NOT_NULL, "REFERENCES xml_metadata(id)"},
-        {"md_parent_id", "integer", I(0), SQL_NOT_NULL, "REFERENCES xml_metadata(id)"}
+        {"md_file_id", "integer", N, SQL_NOT_NULL, "REFERENCES xml_metadata(id)"},
+        {"md_parent_id", "integer", N, 0, "REFERENCES xml_metadata(id)"}
 };
 table_info_t metadata_reference = {
         "metadata_reference",
@@ -143,8 +142,8 @@ table_info_t metadata_reference = {
 };
 
 static column_info_t manifest_columns[] = {
-        {"id", "text", T("OWC"), SQL_PRIMARY_KEY, NULL},
-        {"manifest", "text", T(""), SQL_NOT_NULL, NULL}
+        {"id", "text", N, SQL_PRIMARY_KEY, NULL},
+        {"manifest", "text", N, SQL_NOT_NULL, NULL}
 };
 table_info_t manifest = {
         "manifest",
