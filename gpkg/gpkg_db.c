b5102f720b375b5642edbcf80863006a8e204947
Pepijn Van Eeckhoudt <pepijn.vaneeckhoudt@luciad.com>
2013-09-27 23:17:29 +0200

    Add default srs row for WGS 84

--- gpkg/gpkg_db.c
+++ gpkg/gpkg_db.c
@@ -35,7 +35,8 @@ static column_info_t gpkg_spatial_ref_sys_columns[] = {
 };
 static value_t gpkg_spatial_ref_sys_data[] = {
   T("Undefined Cartesian"), I(-1), T("NONE"), I(-1), T("undefined"), N,
-  T("Undefined Geographic"), I(0), T("NONE"), I(0), T("undefined"), N
+  T("Undefined Geographic"), I(0), T("NONE"), I(0), T("undefined"), N,
+  T("WGS 84"), I(4326), T("EPSG"), I(4326), T( "GEOGCS[\"WGS 84\", DATUM[\"WGS_1984\", SPHEROID[\"WGS 84\",6378137,298.257223563, AUTHORITY[\"EPSG\",\"7030\"]], AUTHORITY[\"EPSG\",\"6326\"]],PRIMEM[\"Greenwich\",0,AUTHORITY[\"EPSG\",\" 8901\"]],UNIT[\"degree\",0.0174532925199433,AUTHORITY[\"EPSG\",\"9122\"]], AUTHORITY[\"EPSG\",\"4326\"]]" ), N
 };
 static table_info_t gpkg_spatial_ref_sys = {
   "gpkg_spatial_ref_sys",
