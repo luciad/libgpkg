c34dac292a5931398dd0a73f9c70589ab432f978
Pepijn Van Eeckhoudt <pepijn.vaneeckhoudt@luciad.com>
2013-09-29 22:19:25 +0200

    Change check order in UseTLS.cmake to avoid using declspec(thread) with clang

--- gpkg/cmake/UseTLS.cmake
+++ gpkg/cmake/UseTLS.cmake
@@ -19,21 +19,6 @@ function(check_tls)
   endif()
 
   if( NOT ___TLS )
-    message( STATUS "Checking if __declspec( thread ) is supported" )
-    check_c_source_compiles(
-      "__declspec( thread ) int i; int main(int argc, char **argv) { return 1; }"
-      TLS_USE_DECLSPEC_THREAD
-    )
-    set( ___TLS ${TLS_USE_DECLSPEC_THREAD} )
-    if (${TLS_USE_DECLSPEC_THREAD})
-      message( STATUS "Checking if __declspec( thread ) is supported - yes" )
-      set( TLS_USE_DECLSPEC_THREAD ${TLS_USE_DECLSPEC_THREAD} PARENT_SCOPE )
-    else()
-      message( STATUS "Checking if __declspec( thread ) is supported - no" )
-    endif()
-  endif()
-
-  if( NOT ___TLS )
     message( STATUS "Checking if pthread_getspecific is supported" )
     find_package ( Threads )
     if ( CMAKE_USE_PTHREADS_INIT )
@@ -45,6 +30,21 @@ function(check_tls)
       set( TLS_USE_PTHREAD ${TLS_USE_PTHREAD} PARENT_SCOPE )
     else()
       message( STATUS "Checking if pthread_getspecific is supported - no" )
+    endif()
+  endif()
+
+  if( NOT ___TLS )
+    message( STATUS "Checking if __declspec( thread ) is supported" )
+    check_c_source_compiles(
+      "__declspec( thread ) int i; int main(int argc, char **argv) { return 1; }"
+      TLS_USE_DECLSPEC_THREAD
+    )
+    set( ___TLS ${TLS_USE_DECLSPEC_THREAD} )
+    if (${TLS_USE_DECLSPEC_THREAD})
+      message( STATUS "Checking if __declspec( thread ) is supported - yes" )
+      set( TLS_USE_DECLSPEC_THREAD ${TLS_USE_DECLSPEC_THREAD} PARENT_SCOPE )
+    else()
+      message( STATUS "Checking if __declspec( thread ) is supported - no" )
     endif()
   endif()
 
