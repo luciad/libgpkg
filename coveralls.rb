47d99502c8aeb61c5ece8c07e76bdf6383656420
Pepijn Van Eeckhoudt <pepijn.vaneeckhoudt@luciad.com>
2014-01-09 14:32:53 +0100

    Correct newlines in source code Correct execution count for lines with no executable code Correct execution count for unexecuted lines

--- coveralls.rb
+++ coveralls.rb
@@ -47,7 +47,7 @@ class Coveralls
 
     report = {
         'service_name' => 'travis-ci',
-        'service_job_id' => ENV['TRAVIS_JOB_ID'],
+        'service_job_id' => ENV['TRAVIS_JOB_ID'] || '16651305',
         'git' => {
             'head' => {
                 'id' => gitlog('%H'),
@@ -60,42 +60,60 @@ class Coveralls
             'branch' => ENV['TRAVIS_BRANCH'] || gitbranch,
             'remotes' => gitremotes
         },
+        'source_files' => source_files,
         'run_at' => Time.now.utc.to_s
     }
 
-    pp report if verbose?
+    puts JSON.pretty_generate(report) if verbose?
 
     report['source_files'] = source_files
+    File.open('report.json', 'w') { |f| JSON.dump( report, f ) }
+    'report.json'
+  end
 
-    JSON.dump(report)
+  def report(gcov_file, root_dir)
+    source_file = ''
+    src = ''
+    cov = []
+    gcov_line = /([^\:]+)\:([^\:]+)\:(.*)/
+    File.foreach(gcov_file) do |line|
+      match = gcov_line.match(line)
+      next unless match
+
+      exec_count = match[1].strip
+      line_no = match[2].strip.to_i
+      source_line = match[3]
+      if line_no == 0
+        tag,info = source_line.split(':', 2)
+        source_file = info.strip if tag == 'Source'
+      else
+        src << "\n"
+        src << source_line
+        if exec_count.index('-')
+          cov << nil
+        else
+          cov << exec_count.to_i
+        end
+      end
+    end
+
+    {
+        'name' => Pathname.new(source_file).relative_path_from(root_dir).to_s,
+        'source' => src,
+        'coverage' => cov
+    }
   end
 
   def post_report(report)
-    url = URI('https://coveralls.io/api/v1/jobs')
-    req = Net::HTTP::Post.new(url)
-    req.content_type = 'multipart/form-data; boundary=AgBfCeDdEcFbGa'
-
-    multipart_data = "--AgBfCeDdEcFbGa\n"
-    multipart_data << "Content-Disposition: form-data; name=\"json_file\"\r\n"
-    multipart_data << "Content-Type: application/json\r\n"
-    multipart_data << "\r\n"
-    multipart_data << report
-    multipart_data << "\r\n"
-    multipart_data << "--AgBfCeDdEcFbGa--\r\n"
-
-    req.body = multipart_data
-    res = Net::HTTP.start(url.hostname, url.port, :use_ssl => url.scheme == 'https' ) do |http|
-      http.request(req)
-    end
-
-    if verbose?
-      puts "Coveralls.io reponded with HTTP #{res.code}"
-      puts res.message
-    end
+    system('curl', "-Fjson_file=@#{report}", 'https://coveralls.io/api/v1/jobs')
   end
 
   def verbose?
     @verbose
+  end
+
+  def debug?
+    true
   end
 
   def gitlog(fmt)
@@ -116,39 +134,6 @@ class Coveralls
 
   def run_gcov(object_file)
     `gcov #{object_file}`
-  end
-
-  def report(gcov_file, root_dir)
-    source_file = ''
-    src = ''
-    cov = []
-    gcov_line = /([^\:]+)\:([^\:]+)\:(.*)/
-    File.foreach(gcov_file) do |line|
-      match = gcov_line.match(line)
-      next unless match
-
-      exec_count = match[1].strip
-      line_no = match[2].strip.to_i
-      source_line = match[3]
-      if line_no == 0
-        tag,info = source_line.split(':', 2)
-        source_file = info.strip if tag == 'Source'
-      else
-        src << '\n' unless src.empty?
-        src << source_line
-        if exec_count.index('#') || exec_count.index('=')
-          cov << nil
-        else
-          cov << exec_count.to_i
-        end
-      end
-    end
-
-    {
-      'name' => Pathname.new(source_file).relative_path_from(root_dir).to_s,
-      'source' => src,
-      'coverage' => cov
-    }
   end
 end
 
