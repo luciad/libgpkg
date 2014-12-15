a477c66708d63fb7e8fb7643e137eecf365dc474
Erik De Weerdt <erik.deweerdt@luciad.com>
2014-12-15 09:44:43 +0100

    added gpkg shell to build (commented out, segfault at startup)

--- Android.mk
+++ Android.mk
@@ -34,3 +34,25 @@ LOCAL_CFLAGS := \
     -DGPKG_EXPORT="__attribute__((visibility(\"default\")))"
 
 include $(BUILD_SHARED_LIBRARY)
+
+# Build the shell for android
+# To link the shared lib, add its path to LD_LIBRARY_PATH
+# Currently segfaults when trying to run. No idea why yet.
+#
+#include $(CLEAR_VARS)
+#
+#LOCAL_SRC_FILES := shell/shell.c
+#
+#LOCAL_C_INCLUDES := \
+#    $(LOCAL_PATH)/sqlite \
+#    $(LOCAL_PATH)/gpkg
+#
+#LOCAL_MODULE := gpkg_shell
+#
+#LOCAL_STATIC_LIBRARIES := sqlite
+#LOCAL_SHARED_LIBRARIES := gpkg
+#
+#LOCAL_CFLAGS += -fPIE
+#LOCAL_LDFLAGS += -fPIE -pie
+#
+#include $(BUILD_EXECUTABLE)
